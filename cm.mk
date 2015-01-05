ec251e6fa9e0a0471fda82a004dfb3a0665361bf
finnq <finnq@finnq.de>
2015-01-05 22:36:59 +0100

    d855: Update cm.mk
    - Fingerprint updated to latest release
    
    Change-Id: I3b732cf2b66042539e88ae90304bd338c749c37e

--- cm.mk
+++ cm.mk
@@ -11,5 +11,5 @@ PRODUCT_NAME := cm_d855
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:4.4.2/KVT49L.A1404795316/1404795316:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 4.4.2 KVT49L.A1404795316 1404795316 release-keys"
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1417604035/1417604035:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1417604035 1417604035 release-keys"
