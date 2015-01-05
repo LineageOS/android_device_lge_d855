2f6553b221183882565467cfb07090bbd6fda234
finnq <finnq@finnq.de>
2015-01-05 22:36:59 +0100

    d855: Update cm.mk
    - Added PRODUCT_DEVICE, PRODUCT_RELEASE_NAME, PRODUCT_BRAND, PRODUCT_MODEL, PRODUCT_MANUFACTURER
    - Custom fingerprint removed because it is useless in my eyes and falls into oblivion.
    
    Change-Id: I3b732cf2b66042539e88ae90304bd338c749c37e

--- cm.mk
+++ cm.mk
@@ -6,10 +6,13 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+PRODUCT_DEVICE := d855
 PRODUCT_NAME := cm_d855
+PRODUCT_RELEASE_NAME := G3
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-D855
+PRODUCT_MANUFACTURER := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
-    PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:4.4.2/KVT49L.A1404795316/1404795316:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 4.4.2 KVT49L.A1404795316 1404795316 release-keys"
+    PRODUCT_NAME="g3_global_com"
