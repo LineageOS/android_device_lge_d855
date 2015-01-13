3de451c16462b2d09c3293fe885367ae16ab7dda
AngelBob <cambercreak@gmail.com>
2015-01-13 19:12:58 +0000

    D855: Update fingerprint to LGD855AT-00-V20f-GLOBAL-COM-DEC-22-2014+0
    Change-Id: I939791f2cd4b7814dbd4fb604dd7af3d0e8d51db

--- cm.mk
+++ cm.mk
@@ -11,5 +11,5 @@ PRODUCT_NAME := cm_d855
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1417604035/1417604035:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1417604035 1417604035 release-keys"
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1419207951/1419207951:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1419207951 1419207951 release-keys"
