d80948553344c7462f63ccee54602bf98c33ee5e
Dan Pasanen <dan.pasanen@gmail.com>
2015-07-13 23:11:52 -0500

    recovery: fix parsing multiple baseband versions
    Change-Id: I8197cf2345a70a78f149846235bac327db2bf973

--- recovery/recovery_updater.c
+++ recovery/recovery_updater.c
@@ -161,11 +161,11 @@ Value * VerifyBasebandFn(const char *name, State *state, int argc, Expr *argv[])
                 name, ret);
     }
 
-    for (i = 1; i <= argc; i++) {
-        ret = ReadArgs(state, argv, i, &baseband_version);
-        if (ret < 0) {
+    for (i = 0; i < argc; i++) {
+        baseband_version = Evaluate(state, argv[i]);
+        if (baseband_version < 0) {
             return ErrorAbort(state, "%s() error parsing arguments: %d",
-                name, ret);
+                name, baseband_version);
         }
 
         uiPrintf(state, "Comparing BASEBAND version %s to %s",
