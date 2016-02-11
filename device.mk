fa8a6912e05ae2b7cda66fea28f26cdc1d6314a2
Hendrik Hagendorn <git@finnq.de>
2016-02-11 07:24:40 -0800

    d855: audio: decommonize audio_platform_info.xml
    Change-Id: I3d72f8476d239e1a8707a6cb6734c62df4cb0c02

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ $(call inherit-product-if-exists, vendor/lge/d855/d855-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
+    device/lge/g3-common/configs/audio/audio_platform_info_qcwcn.xml:system/etc/audio_platform_info.xml \
     device/lge/g3-common/configs/audio/mixer_paths_qcwcn.xml:system/etc/mixer_paths.xml
 
 # NFC
