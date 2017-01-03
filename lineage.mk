f65dfbc88612296e705777da89bc81adf50ae919
Kasual <mushroomhead52e@gmail.com>
2017-01-03 22:11:08 +0300

    d855: rebrand to Lineage
    Change-Id: Ibd0b89b31f4568d8526645adb5eacc76c85e958a

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_d855
+PRODUCT_NAME := lineage_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
