1e850648054251f5699ccf18e37c5cc7c81eec50
Hendrik Hagendorn <git@finnq.de>
2017-01-06 23:18:42 +0100

    d855: lunch target: cm->lineage
    Change-Id: Ibca44c03a84cca59420db9a5e12108e741fe4e7e

--- lineage.mk
+++ lineage.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/lge/d855/full_d855.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_d855
+PRODUCT_NAME := lineage_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
