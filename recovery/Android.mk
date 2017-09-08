ed61b79d491cae739577c80857b672539090a1e6
ZjemCiKolege <frager1911@gmail.com>
2017-09-08 20:27:04 +0000

    recovery: update for O compatability
    Change-Id: I9cb7762c2e124514cc0d6c9fb8ffcdfda914f086

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,8 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery) \
+$(call project-path-for,recovery)/updater/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_g3
 LOCAL_MODULE_TAGS := eng
 include $(BUILD_STATIC_LIBRARY)
+
