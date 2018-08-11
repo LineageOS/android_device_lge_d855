1de4c24554e3a601be636bb3e1431e89aca37bbc
Bruno Martins <bgcngm@gmail.com>
2018-08-11 21:44:52 +0200

    d855: Move fstab to /vendor
    Change-Id: Ie1c6cb662f2f54c7c14d3e7d55aedbfbeef5f8a7

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,5 +7,5 @@ LOCAL_MODULE            := fstab.g3
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/fstab.g3
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
