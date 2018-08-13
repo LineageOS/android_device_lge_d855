6ee8d0e595fdde563d96dd73f2d2caad3b992e73
Brinly Taylor <uberlaggydarwin@gmail.com>
2018-08-14 00:16:55 +0200

    d855: remove languages-full.mk
     * full_base_telephony.mk already includes language pack
    
    Change-Id: I0e28542250d6cfe80b6649add486876780754ca3

--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lge/d855/d855-vendor.mk)
 
