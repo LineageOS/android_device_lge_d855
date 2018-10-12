0e813e7ecf013d92c88975def81b9afb02262354
Michael Bestas <mkbestas@lineageos.org>
2018-10-12 12:47:52 +0200

    d855: Move NFC permissions to vendor
    Change-Id: Idf4022e03ba376f7971d02be973444e70dbc4521

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
